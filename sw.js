5214c3f1fbc5b4cbd6253726dde0d1d85d732e38
Saffat Sharif <saffat.sharif05@gmail.com>
2025-07-18 10:49:02 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-75e7e892096b62673f07.js"
+    "url": "webpack-runtime-1e3cfa968c1f6d83a2c4.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "db48778e24b58701b1a22d040bfff9d6"
+    "revision": "79c57b17ee223d310659bfa2162b52f2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
